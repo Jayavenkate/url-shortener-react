d18f2b408232c0958fd7f861ed245261c174046c
Jayavenkate <jayajaikithuja@gmail.com>
2023-05-13 22:50:50 +0300

    url shortener

--- src/UrlShortener.jsx
+++ src/UrlShortener.jsx
@@ -1,16 +1,35 @@
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { Card, CardActions } from "@mui/material";
+import { useFormik } from "formik";
+import * as yup from "yup";
 export function UrlShortener() {
+  const { values, handleChange, handleBlur, handleSubmit, touched, errors } =
+    useFormik({
+      initialValues: {
+        url: "",
+      },
+    });
   return (
-    <Card className="url-short" elevation={3}>
-      <TextField label="Long Url" placeholder="PasteUrl" variant="outlined" />
-     <CardActions className="btn">
-     <Button  variant="contained">
-        Contained
-      </Button>
-     </CardActions>
-      
-    </Card>
+    <form onSubmit={handleSubmit}>
+      <Card className="url-short" elevation={3}>
+        <TextField
+          name="url"
+          value={values.url}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          label="Long Url"
+          placeholder="Paste Url"
+          variant="outlined"
+          error={touched.url && errors.url}
+          helperText={touched.url && errors.url ? errors.url : null}
+        />
+        <CardActions className="btn">
+          <Button type="submit" variant="contained">
+            Contained
+          </Button>
+        </CardActions>
+      </Card>
+    </form>
   );
 }
