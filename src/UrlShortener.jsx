5996bc3adf6e63e0740de15fba6806ab1e1737fc
Jayavenkate <jayajaikithuja@gmail.com>
2023-08-20 14:18:33 +0300

    shorten

--- src/UrlShortener.jsx
+++ src/UrlShortener.jsx
@@ -1,103 +1,110 @@
-// import TextField from "@mui/material/TextField";
-// import Button from "@mui/material/Button";
-// import { Card, CardActions } from "@mui/material";
-// import { useFormik } from "formik";
-// import * as yup from "yup";
-// import { useEffect, useState } from "react";
-// import { API } from "../global";
-// // import { ShortUrl } from "./ShortUrl";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { Card, CardActions } from "@mui/material";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { useEffect, useState } from "react";
+import { API } from "../global";
 
-import { useState } from "react";
+const formValidationSchema = yup.object({
+  url: yup.string().url().required("Enter Valid Url"),
+});
 
-// const formValidationSchema = yup.object({
-//   url: yup.string().url().required("Enter Valid Url"),
-// });
 
-// export function UrlShortener() {
-//   const [data, setData] = useState([]);
-//   const { values, handleChange, handleBlur, handleSubmit, touched, errors } =
-//     useFormik({
-//       initialValues: {
-//         url: "",
-//       },
-//       validationSchema: formValidationSchema,
-//       onSubmit: (values) => {
-//         console.log(values);
-//         fetch(`${API}/create`, {
-//           method: "POST",
-//           body: JSON.stringify(longUrl),
-//           headers: { "content-type": "application/json" },
-//         })
-//           // .then(() => getUrl());
-//           // const getUrl = () => {
-//           //   fetch(`${API}/url`)
-//           .then((res) => res.json())
-//           .then((data) => setData(data));
-//         // };
-//         // useEffect(() => {
-//         //   getUrl();
-//         // }, []);
-//       },
-//     });
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <Card className="url-short" elevation={3}>
-//         <TextField
-//           name="url"
-//           // value={values.url}
-//           value={values.url}
-//           onChange={handleChange}
-//           onBlur={handleBlur}
-//           label="Long Url"
-//           placeholder="Paste Url"
-//           variant="outlined"
-//           error={touched.url && errors.url}
-//           helperText={touched.url && errors.url ? errors.url : null}
-//         />
-//         <CardActions className="btn">
-//           <Button type="submit" variant="contained">
-//             Submit
-//           </Button>
-//         </CardActions>
-//       </Card>
-//       <div>
-//         {/* {data.map((dt) => (
-//           <ShortUrl key={dt._id} shortUrl={dt.short_url} />
-//         ))} */}
-//       </div>
-//     </form>
-//   );
-// }
-
-// // function ShortUrl({ shortUrl }) {
-// //   return (
-// //     <Card className="short-link" elevation={3}>
-// //       <h2>Shorten Link</h2>
-
-// //       <a href={`${API}/${shortUrl}`}>{`${API}/${shortUrl}`}</a>
-// //     </Card>
-// //   );
-// // }
-
-export function UrlShortener() {
-  const [longUrl, setLongUrl] = useState("");
-  return (
-    <div>
-      <h1>Url Shortener</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="url-short">
-          <input
-            type="text"
-            placeholder="Enter the URL"
-            value={longUrl}
-            onChange={(e) => setLongUrl(e.target.value)}
-          />
-          <button className="btn" type="submit">
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
-  );
+function checkFunction(response) {
+  if (response.status === 401) {
+    throw Error("unauthorized");
+  } else {
+    return response.json();
+  }
 }
+export const UrlShortener = () => {
+  const [data, setData] = useState([]);
+  const [show, setShow] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const { handleSubmit, handleChange, handleBlur, values, touched, errors } =
+    useFormik({
+      initialValues: {
+        url: "",
+      },
+      validationSchema: formValidationSchema,
+      onSubmit: (values) => {
+        fetch(`${API}/shorten`, {
+          method: "POST",
+          body: JSON.stringify(values),
+          headers: {
+            "content-type": "application/json",
+          },
+        }).then(() => getUrls());
+
+        const getUrls = () => {
+          fetch(`${API}/urls`)
+            .then((response) => checkFunction(response))
+            .then((data) => setData(data));
+        };
+        useEffect(() => {
+          if (show) {
+            getUrls();
+          }
+        }, [show]);
+      },
+    });
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Card className="input-container">
+        <TextField
+          name="url"
+          value={values.url}
+          onChange={handleChange}
+          label="Long Url"
+          placeholder="Paste Url"
+          variant="outlined"
+          onBlur={handleBlur}
+          error={touched.url && errors.url}
+          helperText={touched.url && errors.url ? errors.url : null}
+        />
+        <CardActions className="btn-actions">
+          <Button
+            onClick={() => {
+              setShow(true);
+            }}
+            color="success"
+            variant="contained"
+            type="submit"
+          >
+            Shorten
+          </Button>
+        </CardActions>
+      </Card>
+      <div>
+        {data.map((ele) => (
+          <Url
+            key={ele._id}
+            shortUrl={ele.short_url}
+            count={count}
+            setCount={setCount}
+          />
+        ))}
+      </div>
+    </form>
+  );
+};
+const Url = ({ shortUrl, count, setCount }) => {
+  return (
+    <Card className="url-container">
+      <span className="url-container-subhead">Shorten Link</span>
+      <a href={`${API}/${shortUrl}`}>{`${API}/${shortUrl}`}</a>
+      <Badge badgeContent={count} className="touch-icon" color="primary">
+        <IconButton
+          sx={{ padding: 0 }}
+          onClick={() => setCount(count + 1)}
+          aria-label="Touch-icon"
+        >
+          <TouchAppIcon />
+        </IconButton>
+      </Badge>
+    </Card>
+  );
+};
